95b84743c81042288fd4906f86ffcfe3412d2979
Ryan Conley <ryandeanconley2@yahoo.com>
2021-02-22 14:25:17 -0600

    correction to uri

--- src/constance/URI.js
+++ src/constance/URI.js
@@ -1,3 +1,3 @@
-const backendURI = 'postgres://inhjayoelppvqd:9561d42deee587ac13a0973ad2a01b2ba2d6a139cb4ce45ff49fd857de1ca2d4@ec2-3-95-85-91.compute-1.amazonaws.com:5432/dbr3eu1rmch5gk';
+const backendURI = 'https://league-of-leagues-backend.herokuapp.com/';
 
 export default backendURI;
