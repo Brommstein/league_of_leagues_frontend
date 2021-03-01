6f5af93a8e331c1c6e1e840dbdcbf13f4ea94ba2
Ryan Conley <ryandeanconley2@yahoo.com>
2021-02-28 23:02:45 -0600

    fixed login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -55,22 +55,23 @@ const LoginForm = () => {
     });
 
     return (
-        <div className="border">
-            {!userStatus && <div>
-                <form name="login" onSubmit={onSubmit}>
-                    <label htmlFor="username">Username: </label>
-                    <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} required></input>
-                    <p></p>
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} required></input>
-                    <p></p>
-                    <p className='redletter'>{message}</p>
-                    <button type="submit">Login</button>
-                    <Link to="/">Cancel</Link>
-                    <p>Or</p>
-                    <Link to="/createAccount">Create an account</Link>
-                </form>
-            </div>}
+        <div className="form">
+            <section className="border loginform">
+                {!userStatus && <div>
+                    <form name="login" onSubmit={onSubmit}>
+                        <label htmlFor="username">Username: </label>
+                        <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} required></input>
+                        <p></p>
+                        <label htmlFor="password">Password: </label>
+                        <input type="password" id="password" value={password} onChange={e => setPassword(e.target.value)} required></input>
+                        <p></p>
+                        <p className='redletter'>{message}</p>
+                        <button type="submit">Login</button>
+                        <Link to="/">Cancel</Link>
+                        <p>Or <Link to="/createAccount">Create an account</Link></p>
+                    </form>
+                </div>}
+            </section>
             {userStatus && <h2>Logged in as {usernamelogged}</h2>}
             {userStatus && <Link to="/">Home</Link>}
         </div>
